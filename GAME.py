910b05d4fc927107563b64489d3a1b01a3fb760f
KMH8359 <gnslgns2000@naver.com>
2019-10-13 20:24:13 +0900

    점프 구현
    달릴때와 점프할때 프레임만 바꾸는 게 아닌 프레임의 전환 속도도 조정이 필요함

--- GAME.py
+++ GAME.py
@@ -5,27 +5,43 @@ map = load_image('Map_OVEN1.png')
 #if cookienum == 1:
 #character = load_image('BraveCookie.png')
 #elif cookienum == 2:
-character = load_image('ButtercreamCookie.png')
+#character = load_image('ButtercreamCookie.png')
 #elif cookienum == 3:
-#character = load_image('AngelCookie.png')
+character = load_image('AngelCookie.png')
 #elif cookienum == 4:
 #character = load_image('KnightCookie.png')
+#character = load_image('ZombieCookie.png')
 
 running = True
+jumping = False
+sliding = False
 x = 0
+y = 0
+stateframeY = 1300
 frame = 0
 cookienum = 2
+jumpcount = 0
 
 
 def handle_events():
     global running
     global cookienum
+    global y
+    global jumping
+    global stateframeY
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_UP:
+            if jumping == False:
+                jumping = True
+                stateframeY = 10
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_UP:
+            if sliding == False:
+                sliding = True
         elif event.type == SDL_MOUSEBUTTONDOWN:
             cookienum += 1
             if cookienum > 4:
@@ -39,12 +55,26 @@ while True:
     #if cookienum == 1:
     #character.clip_draw(frame * 272 + 10, 1090, 250, 250, x, 200)
     #if cookienum == 2 and cookienum == 3 and cookienum == 4:
-    character.clip_draw(frame * 322 + 10, 1300, 300, 300, x, 200)
+    character.clip_draw(frame * 322 + 10, stateframeY , 300, 300, x, 200 + y)
     update_canvas()
 
     handle_events()
-    frame = (frame + 1) % 4
+    if jumping == False:
+        frame = (frame + 1) % 4
+    if jumping == True:
+        frame = (frame + 1) % 4
     x += 5
+    if jumping == True:
+        if jumpcount < 4:
+            y += 25
+            jumpcount += 1
+        if jumpcount >= 4:
+            y -= 25
+            jumpcount += 1
+        if jumpcount >= 8:
+            jumpcount = 0
+            jumping = False
+            stateframeY = 1300
     delay(0.05)
 
 close_canvas()
